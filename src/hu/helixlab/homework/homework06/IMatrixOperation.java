9915e3376a8e2eb845a68a55b795ece83362ff46
Norman Teibert <norman.teibert@gmail.com>
2017-04-22 21:54:04 +0200

    #3

--- src/hu/helixlab/homework/homework06/IMatrixOperation.java
+++ src/hu/helixlab/homework/homework06/IMatrixOperation.java
@@ -5,4 +5,8 @@ public interface IMatrixOperation {
     void printMatrix(int[][] array);
 
     int[][] initMatrixWithRandomNumbers(int m, int n, int randomMin, int randomMax);
+
+    int[] multiplyWithScalar(int scalar, int[] multiplyInArray);
+
+    int[] sum(int[] sumArrayFirst, int[] sumArraySecond);
 }
